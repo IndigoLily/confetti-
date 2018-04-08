43e9dc8bd0b75242e9d70c8965f124ddf4e306b6
Lily <yulex.42@gmail.com>
2018-04-08 06:40:02 -0300

    Added spatial hashing

--- sketch.js
+++ sketch.js
@@ -2,10 +2,19 @@ const cnv = document.getElementById('cnv'),
         c = cnv.getContext('2d'),
         w = cnv.width  = 512,
         h = cnv.height = 512,
-        r = 5,
-        k = 2,
-        s = 1,
-   points = [];
+        r = 3,
+        k = 3,
+        s = 2**0.5,
+   points = [],
+     hash = [];
+
+for (let x = 0, len = Math.ceil(w/r+1); x < len; ++x) {
+    hash[x] = [];
+
+    for (let y = 0, len = Math.ceil(h/r+1); y < len; ++y) {
+        hash[x][y] = [];
+    }
+}
 
 points.push(new Point(w/2, h/2));
 c.fillRect(points[0].x, points[0].y, s, s);
@@ -17,7 +26,7 @@ function Point(x, y) {
 }
 
 function randDist() {
-    return Math.random() * r + r;
+    return Math.random() * r * r + r;
 }
 
 function randNear(p) {
@@ -26,14 +35,18 @@ function randNear(p) {
     return new Point(p.x + Math.cos(a) * d, p.y + Math.sin(a) * d);
 }
 
-function collides(p1, p2) {
-    if (dist(p1, p2) < r) {
+function collides(point, others) {
+    if (point.x < 0 || point.x >= w || point.y < 0 || point.y >= h) {
         return true;
-    } else if (p1.x < 0 || p1.x >= w || p1.y < 0 || p1.y >= h) {
-        return true;
-    } else {
-        return false;
     }
+
+    for (let i = 0; i < others.length; ++i) {
+        if (dist(point, others[i]) < r) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 function dist(p1, p2) {
@@ -50,14 +63,20 @@ function draw() {
             let test = randNear(it);
             let valid = true;
 
-            for (let jj = 0; jj < points.length; ++jj) {
-                if (jj != i && collides(test, points[jj])) {
-                    valid = false;
-                    break;
+            let others = [];
+            for (let dx = -1; dx < 2; dx++) {
+                let hx = Math.floor(test.x/r) + dx;
+                if (hash[hx] == undefined) continue;
+
+                for (let dy = -1; dy < 2; dy++) {
+                    let hy = Math.floor(test.y/r) + dy;
+                    if (hash[hx][hy] == undefined) continue;
+
+                    others.push(...hash[hx][hy]);
                 }
             }
 
-            if (valid) cands.push(test);
+            if (!collides(test, others)) cands.push(test);
         }
 
         if (cands.length == 0) {
@@ -79,6 +98,7 @@ function draw() {
             continue;
         } else {
             points.push(cand);
+            hash[Math.floor(cand.x/r)][Math.floor(cand.y/r)].push(cand);
             c.fillRect(cand.x, cand.y, s, s);
         }
     }
